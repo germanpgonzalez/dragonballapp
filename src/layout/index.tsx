8209cc25e9290cce1828981c6e915a0e8cb1490c
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2026-03-17 21:14:18 -0300

    Corregido layout

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -7,10 +7,10 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className=" mt-10 mb-10 px-8">{children}</main>
+      <main className="flex-1 mt-5 mb-10 px-8">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
